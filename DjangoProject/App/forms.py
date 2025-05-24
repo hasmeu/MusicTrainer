6bb55eff8813fbbfad53cbc2aaa3a29f0a1d5f9f
hasmeu <smithhaydendwight@gmail.com>
2025-05-24 16:51:04 -0500

    Add files via upload

---
+++ DjangoProject/App/forms.py
@@ -0,0 +1,11 @@
+#Saves to the auth_user table that comes with Django's User Model
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
---
+++ DjangoProject/App/views.py
@@ -0,0 +1,39 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from App.forms import CustomUserCreationForm
+
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world.")
+
+def home(request):
+    return render(request, "App/Home.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(home)
+        else:
+            messages.error(request, "Invalid username or password")
+    return render(request, "App/Login.html")
+
+def signup_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(home)
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "App/Sign_Up.html", {"form": form})
